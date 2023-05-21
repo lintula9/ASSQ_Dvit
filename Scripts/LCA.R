58ec57a6c18575107923a0b0609e8d63c2295d8e
lintula9 <lintula8@gmail.com>
2023-05-21 13:15:49 +0300

    LCA tests.

--- Scripts/LCA.R
+++ Scripts/LCA.R
@@ -1,8 +1,15 @@
 ##### LCA:
+source("Scripts/DataFetch_DataManagement.R")
 
 # Fit LCAs with different k amount of classes. A priori is 3?
-
+lcavars <- c("Rask_S25OHD", "cesd_mean")
+frml <- as.formula(paste(paste("cbind(Rask_S25OHD,cesd_mean)"), " ~ 1"))
+nclass <- 3
 # Compare fits with k classes - choose by AIC, BIC, % assigned criteria.
+
+lcafit <- flexmix(data = df, 
+                  k = nclass, 
+                  formula = frml)
 
 # Visualize classes: curves for each classes.
 
--- Scripts/Libraries.R
+++ Scripts/Libraries.R
@@ -1,10 +1,11 @@
 # Packages.
 
-package_names <- c("haven", "ggplot2", "poLCA")
+package_names <- c("haven", "ggplot2", "flexmix")
 
 for (i in package_names){
   if ( !requireNamespace( i, 
                           quietly = F )) {
+    message("No package", i, ". Installing package.")
     install.packages( i )
   }
   library( i, character.only = TRUE )}

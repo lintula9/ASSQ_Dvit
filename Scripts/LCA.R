0b37041b13f5936d25f4cc87e5056e7ba0666bdc
lintula9 <lintula8@gmail.com>
2023-05-26 13:27:04 +0300

    Additions to PDF creator script.

--- Scripts/LCA.R
+++ Scripts/LCA.R
@@ -2,7 +2,7 @@
 
 source("Scripts/DataFetch_DataManagement.R")
 
-# Fit LCAs with different k amount of classes. 
+# Select variables.
 lcavars <- c("Rask_S25OHD", 
              "Korj_Napa25OHD",
              "S25OHD_12kk",
@@ -13,33 +13,30 @@ lcavars_nonapa <- c("Rask_S25OHD",
                     "S25OHD_24kk")
 lcavars_6to8Included <- c("")
 
-# Visualize realtionships between D-vitamin variables.
+# Visualize relationships between D-vitamin variables.
 
-ggpairs(df[,c(lcavars,"sukupuoli")], 
+ggpairs(na.omit(df[,c(lcavars,"sukupuoli")]), 
         columns = lcavars, 
         mapping = aes(col = factor(sukupuoli)))
-
-  ### ########################################  ###
-  ### These models have no auxiliary variables. ###
-  ### ########################################  ###
 
 # LCA with all d-vitamin concentration measurements.
 
 LatentGaussians <- Mclust( data = na.omit( df[ , lcavars ] ), 
                           G = 1:6 )
-summary(LatentGaussians)
+# LCA without Napa25OHD
 
 LatentGaussians_NoNapa <- Mclust( data = na.omit( df[ , lcavars_nonapa ] ), 
                                  G = 1:6 )
-summary(LatentGaussians_NoNapa)
 
 # Result plots.
 
-pdf( "Figures/LPAFigs.pdf" , 
+pdf( "Figures/LPAFigs.pdf" , # Create PDF.
     pointsize = 12, 
     width = 12, 
     height = 12,
     family = "serif" )
+
+  # Introduction text for results using all measurement :
 plot(type = "n", 
      x = 0:1, y = 0:1, bty='n',
      xaxt='n', yaxt='n', xlab='', ylab='')
@@ -49,7 +46,9 @@ text("1. Plots of 3 latent profiles including:
      \nS25OHD_12kk
      \nS25OHD_24kk",
      x = .5, y = .6, cex = 2)
-plot( LatentGaussians, 
+
+  # Plots/tables using all measurements results:
+plot( LatentGaussians, # Classification plots
       what = "classification")
 par(mfrow = c(2, 2))
 sapply(1:length(lcavars), FUN = function(x) {
@@ -57,20 +56,42 @@ sapply(1:length(lcavars), FUN = function(x) {
   plot( LatentGaussians, 
         what = "classification", x)
 })
+par(mfrow = c(1,1))
+
 grid.arrange(tableGrob(round(
   LatentGaussians$parameters$mean, 1), 
   cols = as.character(1:3)
 ))
+matplot( LatentGaussians$parameters$mean , 
+         type = "l", 
+         ylim = c( 50, 150 ),
+         axes = F,
+         xlab = "Measurement",
+         ylab = "D-vitamin",
+         lty = 1,
+         col = c("blue", 2, 3))
+axis( 1, at = c( 1, 2, 3 , 4) , 
+      labels = lcavars )
+axis( 2, at = seq( 50, 150, length.out = 5 ) )
+legend( x = 3.5, y = 70, 
+        col = c("blue", 2, 3), 
+        lwd = 1, 
+        title = "Class",
+        legend = 1:3)
 
+  # No napa measurement results:
 plot(type = "n", 
      x = 0:1, y = 0:1, bty='n',
      xaxt='n', yaxt='n', xlab='', ylab='')
-text("2. Plots 3 of latent profiles including: 
+  # Introduction text ....
+text("2. Plots of 3 latent profiles including: 
      \nRask_S25OHD 
      \nS25OHD_12kk
      \nS25OHD_24kk",
      x = .5, y = .6, cex = 2)
-plot( LatentGaussians_NoNapa, 
+
+  # Plots/tables using no-napa measurmenet
+plot( LatentGaussians_NoNapa, # Classification plots
       what = "classification")
 par(mfrow = c(2, 2))
 sapply(1:length(lcavars_nonapa), FUN = function(x) {
@@ -78,11 +99,34 @@ sapply(1:length(lcavars_nonapa), FUN = function(x) {
   plot( LatentGaussians_NoNapa, 
         what = "classification", x)
 })
+par(mfrow = c(1,1))
 grid.arrange(tableGrob(round(
   LatentGaussians_NoNapa$parameters$mean, 1), 
   cols = as.character(1:3)
 ))
 
+matplot( LatentGaussians_NoNapa$parameters$mean , 
+         type = "l", 
+         ylim = c( 50, 150 ),
+         axes = F,
+         xlab = "Measurement",
+         ylab = "D-vitamin",
+         lty = 1,
+         col = c("blue", 2, 3))
+axis( 1, at = c( 1, 2, 3 ) , 
+      labels = lcavars_nonapa )
+axis( 2, at = seq( 50, 150, length.out = 5 ) )
+legend( x = 2.5, y = 70, 
+        col = c("blue", 2, 3), 
+        lwd = 1, 
+        title = "Class",
+        legend = 1:3)
+
+  # Additional sex stratified plot
+ggpairs(na.omit(df[,c(lcavars,"sukupuoli")]), 
+        columns = lcavars, 
+        mapping = aes(col = factor(sukupuoli))) + 
+  ggtitle("Sex stratified distributions")
 
 dev.off()
 

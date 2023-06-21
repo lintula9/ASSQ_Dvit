0ff2f9b07a6135a7d3b7c9dc7637941300e8c1f7
lintula9 <lintula8@gmail.com>
2023-06-21 14:33:04 +0300

    BRLM analyses.

--- BRLM.stan
+++ BRLM.stan
@@ -1,15 +1,12 @@
-// Stancode for BRLM.
-
 data {
   int<lower=0> N;
-  int<lower=0> Ncovariates;
-  int Nmeasurements;
-  
+  int<lower=0> NCovariates;
+  int<lower=0> Nmeasurements;
   matrix[N, Nmeasurements] Measurements;
-  matirx[N, Ncovariates] Covariates;
+  matrix[N, NCovariates] Covariates;
   vector[N] ASSQ;
   vector[Nmeasurements] alpha;
-  vector[Nmeasurements] ExpCriticalWeights;
+  vector[Nmeasurements] ExpCriticalWeights[Nmeasurements];
   vector[Nmeasurements] ExpAccumulationWeights;
 }
 
@@ -18,13 +15,13 @@ parameters {
   real Intercept;
   real Delta;
   simplex[Nmeasurements] Weights;
-  vector[Ncovariates] Coefficients;
+  vector[NCovariates] Coefficients;
   real<lower=0> sigma;
 }
 
 transformed parameters {
   vector[N] LinearFunction;
-  LinearFunction = Intercept + Delta * (Measurements*Weights) + (Covariates * Coefficients)
+  LinearFunction = Intercept + Delta * (Measurements * Weights) + (Covariates * Coefficients);
 }
 
 model {
@@ -32,18 +29,28 @@ model {
   Weights ~ dirichlet(alpha);
   Delta ~ normal(0, .5);
   sigma ~ exponential(1);
-  for(i in 1:Ncovariates){
+  for(i in 1:NCovariates){
     Coefficients[i] ~ normal(0, .5); 
   }
   
   ASSQ ~ normal(LinearFunction, sigma);
 
-
 }
 
 generated quantities {
-  vector[Nmeasurements+1] EuclideanDistances;
-  vector[N] logLikelihood;
-  vector[N] 
+  vector[ Nmeasurements + 1 ] EuclideanDistances;
+  vector[ N ] logLikelihood;
+  vector[ N ] yrep;
+  vector[ N ] residuals;
   
+  for( i in 1:Nmeasurements){
+    EuclideanDistances[ i ] = distance( Weights, ExpCriticalWeights[ i ] );
+  }
+  EuclideanDistances[ Nmeasurements + 1 ] = distance( Weights, ExpAccumulationWeights );
+  
+  for( n in 1:N ) {
+    logLikelihood[ n ] = normal_lpdf( ASSQ[ n ] | LinearFunction[ n ], sigma );
+    yrep[ n ] = normal_rng( LinearFunction[ n ], sigma );
+    residuals[ n ] = yrep[ n ] - ASSQ[ n ];
+  }
 }
--- Scripts/BayesRLM.R
+++ Scripts/BayesRLM.R
@@ -6,39 +6,96 @@ library(rstan)
 bayesVars <- c( "S25OHD_12kk" , "S25OHD_24kk" , "D25OHD_nmol_l_6to8" , # Toddlerhood, infancy and pre-school.
                 "ASSQ_6to8_mean", "sukupuoli" , "ikäASSQ" ) 
 bayesdf <- na.exclude( df[ , bayesVars ] )
+Covariates <- bayesdf[ , c("ikäASSQ", "sukupuoli")]
+names(Covariates) <- c("Age", "Sex")
+Covariates <- as.matrix(Covariates)
 
 stan_data = list(
   
   # D-vitamin variables:
-  Toddlerhood = scale(bayesdf$S25OHD_12kk),
-  Infancy = scale(bayesdf$S25OHD_24kk),
-  Childhood = scale(bayesdf$D25OHD_nmol_l_6to8),
-  measurements = cbind(cbind(Toddlerhood, Infancy), Childhood),
-  Nmeasurements = ncol(measurements),
+  Measurements = matrix(cbind(cbind(
+    scale( bayesdf$S25OHD_12kk ), 
+    scale( bayesdf$S25OHD_24kk )),
+    scale( bayesdf$D25OHD_nmol_l_6to8 )),
+                        ncol = 3, 
+                        dimnames = list(NULL, c("Toddlerhood", "Infancy", "Childhood"))),
+  Nmeasurements = 3,
   
   # Covariates:
-  Age = scale(bayesdf$ikäASSQ),
-  Sex = bayesdf$sukupuoli,
-  Covariates = cbind(Age,Sex),
+  Covariates = Covariates,
   NCovariates = ncol(Covariates),
   
   # Y outcome:
-  ASSQ = scale(bayesdf$ASSQ_6to8_mean),
+  ASSQ = as.vector(scale( bayesdf$ASSQ_6to8_mean )),
   
   # N:
-  N = dim(bayesdf)[1],
+  N = dim( bayesdf )[ 1 ],
   
   # Prior parameters:
-  alpha = c(1/3, 1/3, 1/3), # Dirichlet priors.
+  alpha = c( 1, 1, 1 ), # Dirichlet priors.
   
   # Expected weights for critical period hypotheses
-  ExpCriticalWeights = list(c(2/3, 1/6, 1/6), c(1/6, 2/3, 1/6), c(1/6, 1/6, 2/3)),
-  ExpAccumulationWeights = c(0, 0, 1)
+  ExpCriticalWeights = list( c( 2/3, 1/6, 1/6 ), c( 1/6, 2/3, 1/6 ), c( 1/6, 1/6, 2/3 ) ),
+  ExpAccumulationWeights = c( 0, 0, 1 )
+  
     )
 
 
 # Set up the model:
 
-stan_model("BRLM.stan", 
-           model_name = "BRLM",
-           auto_write = T)
+if(FALSE){ # To prevent sourcing problems.
+brlmFit <- stan( file = "BRLM.stan", data = stan_data, 
+      chains = 4, iter = 2000, cores = 4 )
+}
+
+# Summarize results:
+if(FALSE) { # To prevent sourcing problems.
+  
+  
+  
+  print(brlmFit, pars = c("Delta","Weights", "Coefficients"))
+  
+  plot(brlmFit, pars = c("Delta","Weights", "Coefficients"))
+  
+  # Access the generated quantities from the stanfit object
+  generated_quantities <- extract(brlmFit)
+
+  par(mfrow = c( 2 , 2 ))
+  hypotheses <- c("Toddlerhood sensitive", "Infancy sensitive", "Childhood sensitive", "Childhood critical")
+  for( i in 1:( ncol( generated_quantities$EuclideanDistances ) ) ){
+    dens <- density( generated_quantities$EuclideanDistances[ , i ] )
+    plot( dens, main = hypotheses[ i ], xlab = "Distance")
+    meanQuant <- mean(generated_quantities$EuclideanDistances[ , i ])
+    segments(x0 = meanQuant, x1 = meanQuant,
+             y0 = 0, y1 = dens$y[which.min( abs( round(dens$x, 4) - round(meanQuant, 4) ) )])
+    
+  }
+  par(mfrow = c(1,1))
+}
+
+# For PDF summary: 
+if(FALSE) { # To prevent sourcing problems.
+pdf( "Figures/BRLMFigs.pdf" , # Create PDF.
+     pointsize = 12, 
+     width = 12, 
+     height = 12,
+     family = "serif" )
+  grid.arrange(tableGrob(round ( 
+    summary ( brlmFit, 
+              pars = c("Delta","Weights", "Coefficients"))$summary[ , c("mean","se_mean", 
+                                                                        "2.5%","50%","97.5%")], 3), 
+    theme = ttheme_minimal()))
+  plot(brlmFit, pars = c("Delta","Weights", "Coefficients"))
+  par(mfrow = c( 2 , 2 ))
+  hypotheses <- c("Toddlerhood sensitive", "Infancy sensitive", "Childhood sensitive", "Childhood critical")
+  for( i in 1:( ncol( generated_quantities$EuclideanDistances ) ) ){
+    dens <- density( generated_quantities$EuclideanDistances[ , i ] )
+    plot( dens, main = hypotheses[ i ], xlab = "Distance")
+    meanQuant <- mean(generated_quantities$EuclideanDistances[ , i ])
+    segments(x0 = meanQuant, x1 = meanQuant,
+             y0 = 0, y1 = dens$y[which.min( abs( round(dens$x, 4) - round(meanQuant, 4) ) )])
+    
+  }
+  par(mfrow = c(1,1))
+  dev.off()
+}

c3131a1fe6300bc547739dc4232621f89122bb17
lintula9 <lintula8@gmail.com>
2023-06-28 18:08:25 +0300

    Bayesian analyses, including censored (latent normal) analysis.

---
+++ BLRM_censored2.stan
@@ -0,0 +1,63 @@
+data {
+  
+  int<lower=0> N;
+
+  int<lower=0> NCovariates;
+  int<lower=0> Nmeasurements;
+  
+  int zeroVal[N];
+  
+  matrix[N, Nmeasurements] Measurements;
+  matrix[N, NCovariates] Covariates;
+  
+  vector[N] ASSQ;
+  real<upper=min(ASSQ)> ASSQ_L;
+  
+  vector[Nmeasurements] alpha;
+  vector[Nmeasurements] ExpCriticalWeights[Nmeasurements];
+  vector[Nmeasurements] ExpAccumulationWeights;
+}
+
+
+parameters {
+  real Intercept;
+  real Delta;
+  simplex[ Nmeasurements ] Weights;
+  vector[ NCovariates ] Coefficients;
+  real<lower=0> sigma;
+}
+
+transformed parameters {
+  vector[ N ] Mu;
+  Mu = Intercept + Delta * ( Measurements * Weights ) + ( Covariates * Coefficients);
+
+}
+
+model {
+  Intercept ~ normal( 0, .5 );
+  Weights ~ dirichlet( alpha );
+  Delta ~ normal( 0, .5 );
+  sigma ~ exponential( 1 );
+  for( i in 1 : NCovariates ){
+    Coefficients[ i ] ~ normal(0, .5); 
+  }
+  
+  for( i in 1 : N ) {
+    if( zeroVal[ i ] == 1 ) target += normal_lcdf( ASSQ_L | Mu[ i ], sigma );
+  }
+  for(i in 1 : N){
+    if( zeroVal[ i ] == 0 ) ASSQ[ i ] ~ normal( Mu[ i ], sigma );
+  }
+    
+}
+
+generated quantities {
+  vector[ Nmeasurements + 1 ] EuclideanDistances;
+
+  for( i in 1:Nmeasurements){
+    EuclideanDistances[ i ] = distance( Weights, ExpCriticalWeights[ i ] );
+  }
+  EuclideanDistances[ Nmeasurements + 1 ] = distance( Weights, ExpAccumulationWeights );
+  
+
+}
--- Scripts/BayesRLM.R
+++ Scripts/BayesRLM.R
@@ -8,7 +8,8 @@ bayesVars <- c( "S25OHD_12kk" , "S25OHD_24kk" , "D25OHD_nmol_l_6to8" , # Toddler
 bayesdf <- na.exclude( df[ , bayesVars ] )
 Covariates <- bayesdf[ , c("ikäASSQ", "sukupuoli")]
 names(Covariates) <- c("Age", "Sex")
-Covariates <- as.matrix(Covariates)
+Covariates$Age <- scale(Covariates$Age)
+Covariates$Sex <- factor(Covariates$Sex, levels = c(1,2), labels = c("Male", "Female"))
 
 stan_data = list(
   
@@ -22,7 +23,7 @@ stan_data = list(
   Nmeasurements = 3,
   
   # Covariates:
-  Covariates = Covariates,
+  Covariates = subset( model.matrix( ~ Age + Sex, data = Covariates) , select = -c(`(Intercept)`) ),
   NCovariates = ncol(Covariates),
   
   # Y outcome:
@@ -44,61 +45,14 @@ stan_data = list(
 # Set up the model:
 
 if(FALSE){ # To prevent sourcing problems.
-brlmFit <- stan( file = "BRLM.stan", data = stan_data, 
+
+  brlmFit <- stan( file = "BRLM.stan", data = stan_data, 
       chains = 4, iter = 2000, cores = 4 )
+  
+  saveRDS(brlmFit, file = "brlmfit")
+  brlmFit <- readRDS("brlmfit")
 }
 
-# Summarize results:
-if(FALSE) { # To prevent sourcing problems.
-  
-  
-  
-  print(brlmFit, pars = c("Delta","Weights", "Coefficients"))
-  
-  plot(brlmFit, pars = c("Delta","Weights", "Coefficients"))
-  
-  # Access the generated quantities from the stanfit object
-  generated_quantities <- extract(brlmFit)
-
-  par(mfrow = c( 2 , 2 ))
-  hypotheses <- c("Toddlerhood sensitive", "Infancy sensitive", "Childhood sensitive", "Childhood critical")
-  for( i in 1:( ncol( generated_quantities$EuclideanDistances ) ) ){
-    dens <- density( generated_quantities$EuclideanDistances[ , i ] )
-    plot( dens, main = hypotheses[ i ], xlab = "Distance")
-    meanQuant <- mean(generated_quantities$EuclideanDistances[ , i ])
-    segments(x0 = meanQuant, x1 = meanQuant,
-             y0 = 0, y1 = dens$y[which.min( abs( round(dens$x, 4) - round(meanQuant, 4) ) )])
-    
-  }
-  par(mfrow = c(1,1))
-}
-
-# For PDF summary: 
-if(FALSE) { # To prevent sourcing problems.
-pdf( "Figures/BRLMFigs.pdf" , # Create PDF.
-     pointsize = 12, 
-     width = 12, 
-     height = 12,
-     family = "serif" )
-  grid.arrange(tableGrob(round ( 
-    summary ( brlmFit, 
-              pars = c("Delta","Weights", "Coefficients"))$summary[ , c("mean","se_mean", 
-                                                                        "2.5%","50%","97.5%")], 3), 
-    theme = ttheme_minimal()))
-  plot(brlmFit, pars = c("Delta","Weights", "Coefficients"))
-  par(mfrow = c( 2 , 2 ))
-  hypotheses <- c("Toddlerhood sensitive", "Infancy sensitive", "Childhood sensitive", "Childhood critical")
-  for( i in 1:( ncol( generated_quantities$EuclideanDistances ) ) ){
-    dens <- density( generated_quantities$EuclideanDistances[ , i ] )
-    plot( dens, main = hypotheses[ i ], xlab = "Distance")
-    meanQuant <- mean(generated_quantities$EuclideanDistances[ , i ])
-    segments(x0 = meanQuant, x1 = meanQuant,
-             y0 = 0, y1 = dens$y[which.min( abs( round(dens$x, 4) - round(meanQuant, 4) ) )])
-    
-  }
-  par(mfrow = c(1,1))
-  dev.off()
-}
 
 
 # 4 measurements --------------
@@ -106,11 +60,15 @@ pdf( "Figures/BRLMFigs.pdf" , # Create PDF.
 bayesVars2 <- c( "Rask_S25OHD", "S25OHD_12kk" , "S25OHD_24kk" , "D25OHD_nmol_l_6to8" , # Toddlerhood, infancy and pre-school.
                 "ASSQ_6to8_mean", "sukupuoli" , "ikäASSQ" ) 
 bayesdf2 <- na.exclude( df[ , bayesVars2 ] )
-Covariates2 <- bayesdf2[ , c("ikäASSQ", "sukupuoli")]
-names(Covariates2) <- c("Age", "Sex")
-Covariates2 <- as.matrix(Covariates2)
-Measurements2 <- bayesdf2[ , c("Rask_S25OHD", "S25OHD_12kk" , 
-                               "S25OHD_24kk" , "D25OHD_nmol_l_6to8")]
+Covariates2 <- bayesdf2[ , c( "ikäASSQ", "sukupuoli" ) ]
+names(Covariates2) <- c( "Age", "Sex" )
+
+Covariates2$Age <- scale( Covariates2$Age )
+Covariates2$Sex <- factor( Covariates2$Sex, levels = c(1,2), labels = c("Male", "Female") )
+
+
+Measurements2 <- scale(bayesdf2[ , c( "Rask_S25OHD", "S25OHD_12kk" , 
+                               "S25OHD_24kk" , "D25OHD_nmol_l_6to8" ) ])
 
 stan_data2 = list(
   
@@ -119,7 +77,7 @@ stan_data2 = list(
   Nmeasurements = ncol(Measurements2),
   
   # Covariates:
-  Covariates = Covariates2,
+  Covariates = subset( model.matrix(~ Age + Sex, data = Covariates2) , select = -c(`(Intercept)`) ),
   NCovariates = ncol(Covariates2),
   
   # Y outcome:
@@ -144,22 +102,101 @@ stan_data2 = list(
 # Set up the model:
 
 if(FALSE){ # To prevent sourcing problems.
+  
   brlmFit2 <- stan( file = "BRLM.stan", data = stan_data2, 
                    chains = 4, iter = 2000, cores = 4 )
 }
 
-# Summarize results:
+
+
+
+# Censored analysis ---- 
+
+bayesVars3 <- c( "Rask_S25OHD", "S25OHD_12kk" , "S25OHD_24kk" , "D25OHD_nmol_l_6to8" , # Toddlerhood, infancy and pre-school.
+                 "ASSQ_6to8_mean", "sukupuoli" , "ikäASSQ" ) 
+
+
+bayesdf3 <- na.exclude( df[ , bayesVars3 ] )
+
+obsVals <- bayesdf3$ASSQ_6to8_mean != 0
+censVals <- bayesdf3$ASSQ_6to8_mean == 0
+
+Covariates3 <- bayesdf3[ , c( "ikäASSQ", "sukupuoli" ) ]
+names(Covariates3) <- c( "Age", "Sex" )
+Covariates3$Age <- scale( Covariates3$Age )
+Covariates3$Sex <- factor( Covariates3$Sex, levels = c(1,2), labels = c("Male", "Female") )
+
+Covariates3_obs <- Covariates3[ obsVals , ]
+Covariates3_cens <- Covariates3[ censVals , ]
+
+Measurements3 <- scale(bayesdf3[ , c( "Rask_S25OHD", "S25OHD_12kk" , 
+                                      "S25OHD_24kk" , "D25OHD_nmol_l_6to8" ) ])
+
+Measurements3_obs <- Measurements3[ obsVals , ]
+Measurements3_cens <- Measurements3[ censVals, ]
+ASSQ <- as.vector(scale( bayesdf2$ASSQ_6to8_mean ))
+
+stan_data3 = list(
+  
+  # D-vitamin variables:
+  Measurements = Measurements3,
+  Nmeasurements = ncol( Measurements3 ),
+  
+  # Covariates:
+  Covariates = Covariates3,
+  NCovariates = ncol( Covariates3 ),
+  
+  # Y outcome:
+  ASSQ = ASSQ,
+  ASSQ_L = min(ASSQ),
+  
+  # N:
+  N = dim(Covariates3)[ 1 ],
+  N_obs = dim( Covariates3_obs )[ 1 ],
+  N_cens = dim( Covariates3_cens )[ 1 ],
+  
+  # Prior parameters:
+  alpha = c( 1, 1, 1, 1 ), # Dirichlet priors.
+  
+  # Expected weights for critical period hypotheses
+  ExpCriticalWeights = list( c( 2/4, 1/4, 1/4, 1/4 ), 
+                             c( 1/4, 2/4, 1/4, 1/4 ), 
+                             c( 1/4, 1/4, 2/4, 1/4 ),
+                             c( 1/4, 1/4, 1/4, 2/4 )),
+  ExpAccumulationWeights = c( 0, 0, 0, 1 ),
+  
+  # Is the value zero?
+  zeroVal = as.numeric(censVals)
+  
+)
+
+# Set up the model:
+
+if(FALSE){ # To prevent sourcing problems.
+  
+  brlmFit3 <- stan( file = "BLRM_censored2.stan", 
+                    data = stan_data3, 
+                    chains = 4, 
+                    iter = 2000, 
+                    cores = 4 )
+}
+
+# For PDF summary: --------
 if(FALSE) { # To prevent sourcing problems.
   
+  pdf( "Figures/BRLMFigs.pdf" , # Create PDF.
+       pointsize = 12, 
+       width = 12, 
+       height = 12,
+       family = "serif" )
   
-  
-  print(brlmFit2, pars = c("Delta","Weights", "Coefficients"))
-  
-  plot(brlmFit2, pars = c("Delta","Weights", "Coefficients"))
-  
-  # Access the generated quantities from the stanfit object
-  generated_quantities <- extract(brlmFit2)
-  
+  generated_quantities <- extract(brlmFit)
+  grid.arrange(tableGrob(round ( 
+    summary ( brlmFit, 
+              pars = c("Delta","Weights", "Coefficients"))$summary[ , c("mean","se_mean", 
+                                                                        "2.5%","50%","97.5%")], 3), 
+    theme = ttheme_minimal()))
+  plot(brlmFit, pars = c("Delta","Weights", "Coefficients"))
   par(mfrow = c( 2 , 2 ))
   hypotheses <- c("Toddlerhood sensitive", "Infancy sensitive", 
                   "Childhood sensitive", "Childhood critical")
@@ -172,31 +209,81 @@ if(FALSE) { # To prevent sourcing problems.
     
   }
   par(mfrow = c(1,1))
-}
-
-# For PDF summary: 
-if(FALSE) { # To prevent sourcing problems.
-  pdf( "Figures/BRLMFigs_4measures.pdf" , # Create PDF.
-       pointsize = 12, 
-       width = 12, 
-       height = 12,
-       family = "serif" )
+  
+  generated_quantities2 <- extract(brlmFit2)
+  
   grid.arrange(tableGrob(round ( 
-    summary ( brlmFit, 
+    summary ( brlmFit2, 
               pars = c("Delta","Weights", "Coefficients"))$summary[ , c("mean","se_mean", 
                                                                         "2.5%","50%","97.5%")], 3), 
     theme = ttheme_minimal()))
-  plot(brlmFit, pars = c("Delta","Weights", "Coefficients"))
+  plot(brlmFit2, pars = c("Delta","Weights", "Coefficients"))
   par(mfrow = c( 2 , 2 ))
-  hypotheses <- c("Toddlerhood sensitive", "Infancy sensitive", "Childhood sensitive", "Childhood critical")
-  for( i in 1:( ncol( generated_quantities$EuclideanDistances ) ) ){
-    dens <- density( generated_quantities$EuclideanDistances[ , i ] )
-    plot( dens, main = hypotheses[ i ], xlab = "Distance")
-    meanQuant <- mean(generated_quantities$EuclideanDistances[ , i ])
+  hypotheses2 <- c("pregnancy sensitive", "Toddlerhood sensitive", 
+                   "Infancy sensitive", "Childhood sensitive", "Childhood critical")
+  for( i in 1:( ncol( generated_quantities2$EuclideanDistances ) ) ){
+    dens <- density( generated_quantities2$EuclideanDistances[ , i ] )
+    plot( dens, main = hypotheses2[ i ], xlab = "Distance")
+    meanQuant <- mean(generated_quantities2$EuclideanDistances[ , i ])
     segments(x0 = meanQuant, x1 = meanQuant,
              y0 = 0, y1 = dens$y[which.min( abs( round(dens$x, 4) - round(meanQuant, 4) ) )])
     
   }
   par(mfrow = c(1,1))
+  
+  
+  
+  
+  generated_quantities3 <- extract(brlmFit3)
+  
+  grid.arrange(tableGrob(round ( 
+    summary ( brlmFit3, 
+              pars = c("Delta","Weights", "Coefficients"))$summary[ , c("mean","se_mean", 
+                                                                        "2.5%","50%","97.5%")], 3), 
+    theme = ttheme_minimal()))
+  plot(brlmFit3, pars = c("Delta","Weights", "Coefficients"))
+  par(mfrow = c( 2 , 2 ))
+  hypotheses2 <- c("pregnancy sensitive", "Toddlerhood sensitive", 
+                   "Infancy sensitive", "Childhood sensitive", "Childhood critical")
+  for( i in 1:( ncol( generated_quantities3$EuclideanDistances ) ) ){
+    dens <- density( generated_quantities3$EuclideanDistances[ , i ] )
+    plot( dens, main = hypotheses2[ i ], xlab = "Distance")
+    meanQuant <- mean(generated_quantities3$EuclideanDistances[ , i ])
+    segments(x0 = meanQuant, x1 = meanQuant,
+             y0 = 0, y1 = dens$y[which.min( abs( round(dens$x, 4) - round(meanQuant, 4) ) )])
+    
+  }
+  par(mfrow = c(1,1))
+  
+  hist(scale(df$ASSQ_6to8_mean), 
+       probability = T, 
+       xlim = c(-4,4), 
+       breaks = 25, 
+       main = "Sample distribution vs. Estimated distribution",
+       xlab = "ASSQ standardized")
+  roughMeanEst <- mean(summary(brlmFit3, "Mu")$summary[ , "mean"])
+  roughSDEst <- summary(brlmFit3, "sigma")$summary[ , "mean"]
+  lines( dnorm(seq(-5,5, by = 0.1), 
+               mean = roughMeanEst, 
+               sd = roughSDEst),
+         x = seq(-5,5, by = 0.1))
+  lines( dnorm(seq(-5,5, by = 0.1), 
+               mean = mean(scale(bayesdf3$ASSQ_6to8_mean)), 
+               sd = 1),
+         x = seq(-5,5, by = 0.1), 
+         lty = "dashed")
+  legend(x = 2, y = .7,
+         lty = c(1,2), 
+         legend = c("Estimated", "Sample"), 
+         border = "",  
+         bty = "n", 
+         cex = 1, 
+         y.intersp = 1.5, 
+         seg.len = 2, 
+         text.width = .25, x.intersp = .1)
+  
+  
+  
   dev.off()
 }
+
